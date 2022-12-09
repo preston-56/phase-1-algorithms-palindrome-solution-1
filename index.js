25942003c518a79dac437dc3f343012e65a63ea0
Preston <prestonosoro56@gmail.com>
2022-12-09 17:14:32 +0300

    Done

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
 
-/* 
-  Add your pseudocode here
-*/
+  // Write your algorithm here
+  function reverseString(word) {
+    const Arr = word.split("");
+    const reversedArr = Arr.reverse();
+    const reversedWord = reversedArr.join("");
+    return reversedWord;
+  }
+  function isPalindrome(word) {
+    const reversedWord = reverseString(word);
+    if (word === reversedWord) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isPalindrome("racecar");
 
 /*
-  Add written explanation of your solution here
+  The function should return true if the word is a palindrome and false if it is not.
+  
+  Split the word into a string using the string method. Then  take the split array and reverse it using the reverse method . The reverse method only acts on arrays
+  after reversing arrays. Use the `.join()` method to join the array and return the results in the function
 */
 
-// You can run `node index.js` to view these console logs
+
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
